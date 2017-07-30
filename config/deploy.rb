133ca96a7ec83cc5db85617c24afd20a3ef35a0a
helloamos <salut.amos@gmail.com>
2017-07-30 15:06:46 +0000

    Commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.9.0"
 
 set :application, "Netpush"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "https://github.com/helloamos/netpush"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
